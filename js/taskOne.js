a6795cafd639bd05988680f45a11c2837892d0c1
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-18 14:53:18 +0200

    Update taskOne.js

--- js/taskOne.js
+++ js/taskOne.js
@@ -1,3 +1,4 @@
-do {
+while (true) {
   var endterNum = +prompt("Введите число больше 100?", 0);
-} while (endterNum && (endterNum <= 100 || isNumeric(endterNum) === false));
+  if (endterNum > 100 || endterNum === null) break;
+}
